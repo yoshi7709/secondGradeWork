a4b34a5121349083afe2100662b3f27f26cf9970
yoshi@practice <yoshi.forgit@gmail.com>
2011-12-01 22:34:52 +0900

    Manifest:     windowSoftInputMode追加

--- src/com/yoshi/secondegrade/SecondGradeActivity.java
+++ src/com/yoshi/secondegrade/SecondGradeActivity.java
@@ -2,12 +2,23 @@ package com.yoshi.secondegrade;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class SecondGradeActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        LayoutInflater inflater = getLayoutInflater();
+        LinearLayout l = (LinearLayout) inflater.inflate(R.layout.main, null);
+        EditText v = new EditText(getApplicationContext());
+        v.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+        v.requestFocus();
+        l.addView(v);
+
+        setContentView(l);
     }
 }
